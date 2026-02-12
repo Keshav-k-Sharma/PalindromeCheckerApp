a66f714548014b2f6f2385c07bfc422bcec726b6
Keshav-k-Sharma <reach.keshavks@gmail.com>
2026-02-12 10:25:40 +0530

    UC1 : Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+/*
+UC1
+@author Keshav Krishan Sharma
+@version 1.0
+*/
+public class PalindromeCheckerApp{
+        public static void main(String[] args){
+                System.out.println("WELCOME TO Palindrome checker Management System");
+                System.out.println("Version :1.0");
+                System.out.println("System initiaised "+ "successfully");
+        }
+}
