da0b1b17443be5031c914bd432fbd74b396b8dfe
Keshav-k-Sharma <reach.keshavks@gmail.com>
2026-02-19 09:53:12 +0530

    [solved ]UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,11 +3,26 @@ UC1
 @author Keshav Krishan Sharma
 @version 1.0
 */
+import java.util.*;
 public class PalindromeCheckerApp{
         public static void main(String[] args){
                 System.out.println("WELCOME TO Palindrome checker Management System");
                 System.out.println("Version :1.0");
                 System.out.println("System initiaised "+ "successfully");
-                System.out.println("Hi");
+                String Msg="madam";
+                Msg=Msg.toLowerCase();
+                String Reverse = "";
+                char[] chars = Msg.toCharArray();
+                for (int i=0; i< Msg.length();i++) {
+                        Reverse += chars[i];
+                }
+                if (Reverse.equals(Msg)){
+                        System.out.println("Palindrome");
+                }
+                else{
+                        System.out.println("not palindrome");
+                }
+
         }
 }
+
